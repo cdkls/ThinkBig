7fb608d763703ac633e5291f7e75690f9c6a24fd
Satish Kallepalli <satish_kallepalli@gap.com>
2017-10-24 04:28:52 -0700

    adding CORS config

---
+++ src/main/java/com/thinkbig/controller/Helo.java
@@ -0,0 +1,14 @@
+package com.thinkbig.controller;
+
+public class Helo {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
--- src/main/java/com/thinkbig/controller/HomePageController.java
+++ src/main/java/com/thinkbig/controller/HomePageController.java
@@ -1,5 +1,11 @@
 package com.thinkbig.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomePageController {
     
-   @RequestMapping(value ="/welcome", method = RequestMethod.GET)
-    public String welcomeMessage(){
-        return "Think Big";
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(value ="/welcome", method = RequestMethod.GET)
+    public ResponseEntity<List<Helo>> welcomeMessage(){
+        Helo helo=new Helo();
+        helo.setName("Thing Big");
+        List<Helo> hellos=new ArrayList<>();
+        hellos.add(helo);
+        return new ResponseEntity<List<Helo>>(hellos, HttpStatus.OK);
     }
     
    
