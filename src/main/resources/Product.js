aceee3008f32db6e9d2be0aa5abc436a9ee8d660
bvrcj <Chaitanya.Bommaraju@gmail.com>
2017-10-25 22:47:17 -0700

    CDKLS[Chaitanya]:Updated sample dummy products mongo script.

--- src/main/resources/Product.js
+++ src/main/resources/Product.js
@@ -27,4 +27,132 @@ db.Product.insert({
                     "Party", 
                     "Nightware"
                 ]
+                });
+                
+                
+db.Product.insert({
+"_id":"2",
+"productId": "2",
+"categoryId": "2",
+"productName": "Sweaters",
+"productColors": [
+"RED"
+],
+"productDescription": "Wonderful Sweaters",
+"productImages": [
+"images/s1.jpg",
+"images/s2.jpg",
+"images/s3.jpg",
+"images/s4.jpg",
+"images/s5.jpg",
+"images/s6.jpg",
+"images/s7.jpg",
+"images/s8.jpg"
+],
+"productPrice": "$99.00",
+"productOfferPrice": "$55.00",
+"productType": "Hot",
+"productQuantity": 10,
+"productRating": 5,
+"productOcassion": [
+"Festival",
+"Party",
+"Nightware"
+]
+});
+
+              
+db.Product.insert({
+"_id":"3",
+"productId": "3",
+"categoryId": "3",
+"productName": "Shorts & Skirts",
+"productColors": [
+"RED"
+],
+"productDescription": "Wonderful Shorts & Skirts",
+"productImages": [
+"images/s1.jpg",
+"images/s2.jpg",
+"images/s3.jpg",
+"images/s4.jpg",
+"images/s5.jpg",
+"images/s6.jpg",
+"images/s7.jpg",
+"images/s8.jpg"
+],
+"productPrice": "$99.00",
+"productOfferPrice": "$55.00",
+"productType": "Hot",
+"productQuantity": 10,
+"productRating": 5,
+"productOcassion": [
+"Festival",
+"Party",
+"Nightware"
+]
+});
+
+           
+db.Product.insert({
+"_id":"4",
+"productId": "4",
+"categoryId": "4",
+"productName": "Jeans",
+"productColors": [
+"RED"
+],
+"productDescription": "Wonderful Jeans",
+"productImages": [
+"images/s1.jpg",
+"images/s2.jpg",
+"images/s3.jpg",
+"images/s4.jpg",
+"images/s5.jpg",
+"images/s6.jpg",
+"images/s7.jpg",
+"images/s8.jpg"
+],
+"productPrice": "$99.00",
+"productOfferPrice": "$55.00",
+"productType": "Hot",
+"productQuantity": 10,
+"productRating": 5,
+"productOcassion": [
+"Festival",
+"Party",
+"Nightware"
+]
+});
+
+           
+db.Product.insert({
+"_id":"5",
+"productId": "5",
+"categoryId": "5",
+"productName": "Shirts & Tops",
+"productColors": [
+"RED"
+],
+"productDescription": "Wonderful Shirts & Tops",
+"productImages": [
+"images/s1.jpg",
+"images/s2.jpg",
+"images/s3.jpg",
+"images/s4.jpg",
+"images/s5.jpg",
+"images/s6.jpg",
+"images/s7.jpg",
+"images/s8.jpg"
+],
+"productPrice": "$99.00",
+"productOfferPrice": "$55.00",
+"productType": "Hot",
+"productQuantity": 10,
+"productRating": 5,
+"productOcassion": [
+"Festival",
+"Party",
+"Nightware"
+]
 });
